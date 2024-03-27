73d092dc555c81ebc484dcf07b9ec4bd5fc20743
Александр Жданов <canshok89@gmail.com>
2024-03-27 10:41:59 +0500

    Updated parser service

--- app/services/fgis_parser.py
+++ app/services/fgis_parser.py
@@ -6,61 +6,73 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def get_calibration_info(
-        suitability: bool = True,
-        sensor_type: str = '',
-        sensor_number: str = '',
-     ):
-    if sensor_type is None:
-        sensor_type = ''
-    if sensor_number is None:
-        sensor_number = ''
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    wait = WebDriverWait(driver, 20)
-
-    url = f"https://fgis.gost.ru/fundmetrology/cm/results?filter_applicability={suitability}&filter_mi_mitype={sensor_type}&filter_mi_number={sensor_number}&activeYear=%D0%92%D1%81%D0%B5"
-    # url = f"https://fgis.gost.ru/fundmetrology/cm/results?filter_applicability=true&activeYear=%D0%92%D1%81%D0%B5"
-    driver.get(url)
-
-    try:
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="btn btn-primary"]'))).click()
-        wait.until(EC.presence_of_element_located((By.XPATH, '//tbody/tr')))
-        # sleep(5)
-
-        table_rows = driver.find_elements(By.XPATH, '//tbody/tr')
-
-        calibration_dates = [
-            ['Дата', 'Организация', 'Тип СИ', 'Заводской номер']
-        ]
-        for row in table_rows:
-            organization_element = row.find_element(By.XPATH, './td[1]')
-            ci_element = row.find_element(By.XPATH, './td[4]')
-            ci_number = row.find_element(By.XPATH, './td[6]')
-            calibration_date_element = row.find_element(By.XPATH, './td[7]')
-            calibration_dates.append(
-                [
-                    calibration_date_element.text,
-                    organization_element.text,
-                    ci_element.text,
-                    ci_number.text
-                ]
-            )
-
-        return calibration_dates
-    except Exception as e:
-        print("Информация о поверке не найдена.")
-        raise e
-    finally:
-        driver.quit()
+DEFAULT_URL = "https://fgis.gost.ru/fundmetrology/cm/results?filter_applicability=true&activeYear=%D0%92%D1%81%D0%B5"
+PARSING_URL = (
+    "https://fgis.gost.ru/fundmetrology/cm/results?"
+    "filter_applicability={0}"
+    "&filter_mi_mitype={1}"
+    "&filter_mi_number={2}"
+    "&activeYear=%D0%92%D1%81%D0%B5"
+)
 
 
-suitability = True
-sensor_type = "ДН-130"
-# sensor_type = ""
-# sensor_type = None
-sensor_number = None
+class FgisParser:
 
-for row in get_calibration_info(suitability, sensor_type, sensor_number):
-    if row[0] == '':
-        continue
+    def __driver_settings(self) -> webdriver.Chrome:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        return driver
+
+    def get_calibration_info(
+            self, suitability: bool = True, sensor_type: str = '', sensor_number: str = ''
+    ) -> list[list[str]]:
+        if sensor_type is None:
+            sensor_type = ''
+        if sensor_number is None:
+            sensor_number = ''
+
+        driver: webdriver.Chrome = self.__driver_settings(self)
+        url = PARSING_URL.format(suitability, sensor_type, sensor_number)
+        print(url)
+        driver.get(url)
+
+        wait = WebDriverWait(driver, 20)
+
+        try:
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="btn btn-primary"]'))).click()
+            wait.until(EC.presence_of_element_located((By.XPATH, '//tbody/tr')))
+
+            table_rows = driver.find_elements(By.XPATH, '//tbody/tr')
+
+            calibration_dates = [
+                ['Дата', 'Организация', 'Тип СИ', 'Заводской номер']
+            ]
+            for row in table_rows:
+                organization_element = row.find_element(By.XPATH, './td[1]')
+                ci_element = row.find_element(By.XPATH, './td[4]')
+                ci_number = row.find_element(By.XPATH, './td[6]')
+                calibration_date_element = row.find_element(By.XPATH, './td[7]')
+                calibration_dates.append(
+                    [
+                        calibration_date_element.text,
+                        organization_element.text,
+                        ci_element.text,
+                        ci_number.text
+                    ]
+                )
+
+            return calibration_dates
+        except Exception as e:
+            raise (f'Информация о поверке не найдена. Ошибка: {e}')
+        finally:
+            driver.quit()
+
+
+test_fixture = {
+    'self': FgisParser,
+    'suitability': True,
+    'sensor_type': "ДН-130",
+    'sensor_number': 1030,
+}
+
+for row in FgisParser.get_calibration_info(**test_fixture):
     print(row)
